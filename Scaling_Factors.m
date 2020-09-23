ea9889775e4988326bdce5a5eaf8b39b43ea18a3
Maxximuss Cole-André <64218661+mcoleandre@users.noreply.github.com>
2020-09-22 22:14:29 -0400

    Add files via upload

---
+++ Scaling_Factors.m
@@ -0,0 +1,11 @@
+SF = struct('lambda_Fz0', 'value');
+
+SF.lambda_muxy = 'value';
+SF.lambda_muv = 'value';
+SF.lambda_Cy = 'value';
+SF.lambda_Ey = 'value';
+SF.lambda_Kya = 'value';
+SF.lambda_Kygamma = 'value';
+SF.lambda_Mr = 'value';
+SF.lambda_Kzy = 'value';
+SF.lambda_Kz = 'value';
